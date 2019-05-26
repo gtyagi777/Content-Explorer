adbb2d46736810d107932c34c322dc58bd65e73d
Gaurav Tyagi <gauravtyagi777@gmail.com>
2019-05-25 20:31:06 -0700

    v1

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('movies', views.MovieList.as_view(), name = 'Movie List'),
+    path('movie/<int:pk>', views.DetailView.as_view(), name = 'Movie Detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
-
+from django.views.generic import (ListView, DetailView,)
 from core.models import Movie
 
 # Create your views here.
 
 class MovieList(ListView):
     model = Movie 
+
+class MovieDetail(DetailView):
+    model = Movie
